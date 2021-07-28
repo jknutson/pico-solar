0e1ddaf5cd124c0e33d97070390a436b3f89f2d4
John Knutson <john.m.knutson@gmail.com>
2021-07-28 13:47:33 -0500

    fix bug with sample sum

--- ohms_law.c
+++ ohms_law.c
@@ -5,10 +5,9 @@ float voltage_divider(float vin, float r1, float r2) {
 }
 
 float sampled_adc_read(int sample_cnt) {
-  float sample_sum;
+  float sample_sum = 0.0f;
   for(int i = 0; i < sample_cnt; i++) {
-    uint16_t result = adc_read();
-    sample_sum += result;
+    sample_sum += adc_read();
   }
   return sample_sum / sample_cnt;
 }
